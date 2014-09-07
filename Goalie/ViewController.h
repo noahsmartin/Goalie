5292e347ec33951e3b3090835e67c2a415b6da4c
Noah Martin <Noah@Noahs-MacBook-Pro.local>
2014-09-07 09:45:32 -0700

    Initial Commit

---
+++ Goalie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Goalie
+//
+//  Created by Noah Martin on 9/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Goalie/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Goalie
+//
+//  Created by Noah Martin on 9/7/14.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
