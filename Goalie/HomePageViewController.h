668108be0afe4fd85683df77a9b6bedea278b87e
Noah Martin <Noah@Noahs-MacBook-Pro.local>
2014-09-08 21:29:21 -0700

    First commit

---
+++ Goalie/HomePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomePagerDataSource.h
+//  Goalie
+//
+//  Created by Noah Martin on 9/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePageViewController : UIPageViewController <UIPageViewControllerDataSource>
+
+@end
---
+++ Goalie/HomePageViewController.m
@@ -0,0 +1,58 @@
+//
+//  HomePagerDataSource.m
+//  Goalie
+//
+//  Created by Noah Martin on 9/7/14.
+//
+//
+
+#import "HomePageViewController.h"
+
+@interface HomePageViewController()
+
+@property UIViewController* stats;
+@property UIViewController* feed;
+
+@end
+
+@implementation HomePageViewController
+
+-(void)viewDidLoad {
+    UIStoryboard *aStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
+    self.stats = [aStoryboard instantiateViewControllerWithIdentifier:@"Stats"];
+    self.feed = [aStoryboard instantiateViewControllerWithIdentifier:@"Feed"];
+    [self setViewControllers:@[self.stats] direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:^(BOOL finished) {
+        
+    }];
+    [self setDataSource:self];
+}
+
+-(UIViewController*)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
+    if(viewController == self.stats) {
+        return self.feed;
+    }
+    NSLog(@"after: %@", viewController);
+        return nil;
+    
+}
+
+- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController {
+    // The number of items reflected in the page indicator.
+    return 2;
+}
+
+- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController {
+    // The selected item reflected in the page indicator.
+    return 0;
+}
+
+-(UIViewController*)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+    if(viewController == self.feed) {
+        return self.stats;
+    }
+    NSLog(@"before %@", viewController);
+        return nil;
+}
+
+
+@end
